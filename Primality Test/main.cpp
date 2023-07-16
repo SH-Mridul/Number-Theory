c9f13393a9754b1f8e958c9a853423617c31463c
mridulhossain <mridulcse8@gmail.com>
2023-07-17 05:52:43 +0600

    added primality test(prime number check)

--- Primality Test/main.cpp
+++ Primality Test/main.cpp
@@ -17,7 +17,7 @@ bool isPrime(int n){
   if(n<=3) return true;
   if(n%2 == 0) return false;
   
-  for(int i = 3; i<sqrt(n); i++)
+  for(int i = 3; i<sqrt(n); i += 2)
     if(n%i == 0) return false;
   
   return true;
