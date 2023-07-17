bbb5a85ce77b57c9019044a0ef3ed286b5ddd889
mridulhossain <mridulcse8@gmail.com>
2023-07-17 15:42:54 +0600

    added primality test(prime number check)

--- Primality Test/main.cpp
+++ Primality Test/main.cpp
@@ -5,20 +5,27 @@ bool isPrime(int n);
 int main() {
  int n;
  cin>>n;
- cout<<isPrime(n)<<endl;
+ 
+ while(n--){
+  int input;
+  cin>>input;
+  if(isPrime(input)) 
+    cout<<"yes"<<endl;
+  else 
+    cout<<"no"<<endl;
+ }
  
  return 0;
 }
 
 bool isPrime(int n){
-  
-  //corner case;
   if(n<2) return false;
-  if(n<=3) return true;
+  if(n <= 3) return true;
   if(n%2 == 0) return false;
   
-  for(int i = 3; i<sqrt(n); i += 2)
-    if(n%i == 0) return false;
+  for(int i = 2; i*i<=n; i++){
+      if(n%i == 0) return false;
+  }
   
   return true;
 }
