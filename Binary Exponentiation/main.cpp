00d1f05881eefc5a76c1af5469e717c0eab7b301
mridulhossain <mridulcse8@gmail.com>
2023-07-20 05:59:37 +0600

    binary exponentiation added calculation for big numbers

--- Binary Exponentiation/main.cpp
+++ Binary Exponentiation/main.cpp
@@ -6,16 +6,31 @@ int main() {
  int base,exponent;
  cin>>base>>exponent;
  int result = 1;
+ int mod = 1e9;
  
  while(exponent != 0){
  	if(exponent%2 != 0){
- 		result *= base; 
+ 		result = (result*base)%mod; 
  		exponent--;
  	}else{
- 		base *= base;
+ 		base = (base*base)%mod;
  		exponent /= 2;
  	}
  }
+ 
+ 
+ //---------------its for smaller number-------
+ //-------------upper one is safe,when comes borrow--------
+ //  while(exponent != 0){
+ //  if(exponent%2 != 0){
+ //    result *= base; 
+ //    exponent--;
+ //  }else{
+ //    base *= base;
+ //    exponent /= 2;
+ //  }
+ // }
+ 
  cout<<result<<endl;
  return 0;
 }
